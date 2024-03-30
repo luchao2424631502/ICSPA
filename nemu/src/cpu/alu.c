a85f83999caeaa528120e546603f57b984e5e255
tracer-pa-public <tracer@njuics.org>
2024-03-30 20:57:43 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 20:57:43 up 23:27,  4 users,  load average: 0.00, 0.00, 0.00 > unitime > c10a53e9ce6d4be29bf68649fb417214b8b1783

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -178,16 +178,17 @@ uint64_t alu_mul(uint32_t src, uint32_t dest, size_t data_size)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_mul(src, dest, data_size);
 #else
-	uint32_t ans = dest * src;
+	uint64_t ans = dest * src;
+	uint64_t tmp = 0xFFFFFFFF00000000;
 
 	if (data_size == 32) { // ans = edx:eax
 		// 因为是32bit, 只能靠溢出来设置标志位
-		if ((ans / dest) == src) {
-			cpu.eflags.CF = 0;
-			cpu.eflags.OF = 0;
-		} else {
+		if (ans & tmp) {
 			cpu.eflags.CF = 1;
 			cpu.eflags.OF = 1;
+		} else {
+			cpu.eflags.CF = 0;
+			cpu.eflags.OF = 0;
 		}
 	} else if(data_size == 16) { // ans = DX:AX
 		// 判断DX是否全0
