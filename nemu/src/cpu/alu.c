6e997bbab19d1872b5a473c025d40ce5c806ab2a
tracer-pa-public <tracer@njuics.org>
2024-03-30 13:59:50 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 13:59:50 up 16:29,  4 users,  load average: 0.00, 0.00, 0.00 > unitime > e23b241a4c161120f89062cb46c26cea3cfdf70e

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -1,27 +1,18 @@
 #include "cpu/cpu.h"
 
-struct bit_add_ans {
-	uint8_t cn;
-	uint8_t cout;
-};
-
-static inline struct bit_add_ans bit_add(uint8_t a, uint8_t b, uint8_t cf)
-{
-	struct bit_add_ans ans;
-	ans.cout = (a + b + cf) >> 0x1U;
-	ans.cn = (a + b + cf) & 0x1U;
-	return ans;
-}
-
 static inline uint8_t get_bit(uint8_t index, uint32_t num)
 {
-	
-	return (num & (0x1U << index));
+	return (num >> index) & 0x1U;
 }
 
-static inline void set_bit(uint8_t index, uint32_t *num, uint8_t value)
+static inline void set_bit0(uint8_t index, uint32_t *num)
 {
-	*num = *num | (value << index);
+	*num = *num & (~(0x1 << index));
+}
+
+static inline void set_bit1(uint8_t index, uint32_t *num)
+{
+	*num = *num | (0x1 << index);
 }
 
 uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size)
@@ -29,32 +20,27 @@ uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_add(src, dest, data_size);
 #else
-	uint8_t OF, CF, ZF, SF;
-	uint8_t Cn, Cn_1 = 0;
-	uint8_t cin = 0; 
-	uint32_t res = 0;
+	uint8_t ZF, SF, OF, CF;
+	uint8_t fn, cout, cout_1;
+        uint8_t	cin = 0;
+	uint32_t ans = 0;
 
-	for (size_t i = 0; i < 32; i++) {
-		struct bit_add_ans ans = bit_add(get_bit(i, src), get_bit(i, dest), cin);
+	for (int i = 0; i < 32; i++) {
+		fn = (get_bit(i, src) + get_bit(i, dest) + cin) & 0x1;
+		cout = (get_bit(i, src) + get_bit(i, dest) + cin) >> 1;
+		cin = cout;
 
-		set_bit(i, &res, ans.cn); // 保存运算结果
-		cin = ans.cout; // 保存进位值
-		if (i == 30)
-			Cn_1 = cin;
+		(fn == 0) ? set_bit0(i, &ans) : set_bit1(i, &ans);
+		if (30 == i)
+			cout_1 = cout;
 	}
-	Cn = cin;
-	
-	OF = Cn ^ Cn_1;
-	SF = get_bit(31, res);
-	ZF = (res == 0) ? 1 : 0;
-	CF = 0 ^ Cn;
 
-	cpu.eflags.OF = OF;
-	cpu.eflags.SF = SF;
-	cpu.eflags.ZF = ZF;
-	cpu.eflags.CF = CF;	
+	cpu.eflags.ZF = (ans == 0) ? 1 : 0;
+	cpu.eflags.SF = get_bit(31, ans) ? 1 : 0;
+	cpu.eflags.OF = cout ^ cout_1;
+	cpu.eflags.CF = 0 ^ cout;
 
-	return res;
+	return ans;
 #endif
 }
 
