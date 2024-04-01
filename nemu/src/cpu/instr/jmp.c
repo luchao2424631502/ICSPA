a28a672630012a05d7730d8d4c3d501012f87c76
tracer-pa-public <tracer@njuics.org>
2024-04-01 20:31:44 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 20:31:44 up 2 days, 23:01,  5 users,  load average: 0.00, 0.02, 0.02 > unitime > 299d06f9d51e97a9a00efe00cfeae1757724ce4a

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -1,5 +1,6 @@
 #include "cpu/instr.h"
 
+// int jmp_near(uint32_t eip, uint8_t opcode)
 make_instr_func(jmp_near)
 {
         OPERAND rel;
@@ -10,11 +11,11 @@ make_instr_func(jmp_near)
 
         operand_read(&rel);
 
-        int offset = sign_ext(rel.val, data_size);
+        int offset = sign_ext(rel.val, data_size); // 跳转地址本来就是signed编码,
         // thank Ting Xu from CS'17 for finding this bug
         print_asm_1("jmp", "", 1 + data_size / 8, &rel);
 
-        cpu.eip += offset;
+        cpu.eip += offset; // 跳转目标地址是相当于当前指令的地址计算的
 
         return 1 + data_size / 8;
 }
