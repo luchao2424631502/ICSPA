822fc0b1cf0eaa8e058b3a965c38b40045d2b397
tracer-pa-public <tracer@njuics.org>
2024-04-01 22:10:12 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 22:10:12 up 3 days, 39 min,  6 users,  load average: 0.03, 0.05, 0.00 > unitime > 465a5b785aea49a95d691b2faffe74367d79df70

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -17,7 +17,8 @@ make_instr_impl_1op(push, r, l) // push_r_l
 // 每一个宏生成来说, 都给了一个static 用户自己实现的 执行函数
 static void instr_execute_1op()
 {
-	operand_read(&opr_src); // push xxx = mov reg -> [$esp]
-	vaddr_write(cpu.esp, SREG_CS, 4, opr_src.val); 
-	cpu.esp -= 4; // esp -= 4
+	operand_read(&opr_src); // opr_src.val = $ebp的值
+
+	vaddr_write(cpu.esp, SREG_CS, (data_size / 8), opr_src.val);
+	cpu.esp = cpu.esp - (data_size / 8);
 }
