9cfc5205845eeed03c09e3820d50be0f713b3a9b
tracer-pa-public <tracer@njuics.org>
2024-04-01 21:29:47 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 21:29:47 up 2 days, 23:59,  6 users,  load average: 0.12, 0.07, 0.01 > unitime > 5b8a0b69d30bd6cfbb86788fccf46ab93c61d87

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -18,6 +18,6 @@ make_instr_impl_1op(push, r, l)
 static void instr_execute_1op()
 {
 	operand_read(&opr_src); // push xxx = mov reg -> [$esp]
-	vaddr_write((vaddr_t)cpu.eflags.ESP, SREG_CS, data_size, opr_src.val); 
+	vaddr_write((vaddr_t)cpu.eflags.esp, SREG_CS, data_size, opr_src.val); 
 	cpu.eflags.ESP -= data_size; // esp -= 4
 }
