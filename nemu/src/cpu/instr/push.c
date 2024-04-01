c8283d67701dc0a8ff5661461aa29ad94e23e0ba
tracer-pa-public <tracer@njuics.org>
2024-04-01 20:56:58 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 20:56:58 up 2 days, 23:26,  6 users,  load average: 0.03, 0.02, 0.00 > unitime > 1a9545f0b64e2aa9dab8502f39fcb248dcac64b

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -3,9 +3,9 @@
 Put the implementations of `push' instructions here.
 */
 
-make_instr_impl_2op(push, r, rm, l)
+// make_instr_impl_2op(push, r, rm, l)
 
 // 字符串接在 make_instr_impl_1op 中
-static void instr_execute_2op()
-{
-}
+// static void instr_execute_2op()
+// {
+// }
