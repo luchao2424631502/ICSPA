e24571ef873ed7589146db4f03eb17f6db1851ba
tracer-pa-public <tracer@njuics.org>
2024-04-01 22:23:53 +0800

    > clean > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 22:23:53 up 3 days, 53 min,  6 users,  load average: 0.01, 0.04, 0.00 > unitime > 715b51c49f36e49939bc977839e3ff4d962dcba4

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -5,7 +5,7 @@ Put the implementations of `push' instructions here.
 
 static void instr_execute_1op();
 
-make_instr_impl_1op(push, r, l) // push_r_l
+make_instr_impl_1op(push, r, l) // 生成的push_r_l适用于0x50~0x57, 所以需要修改相应的opcode function
 /* 
  * int len = 1;
  * decode_data_size_l
@@ -17,13 +17,10 @@ make_instr_impl_1op(push, r, l) // push_r_l
 // 每一个宏生成来说, 都给了一个static 用户自己实现的 执行函数
 static void instr_execute_1op()
 {
-	cpu.ebp = 0x99;
-	printf("before info reg $ebp=0x%X $esp=0x%X [$esp]=0x%X\n", cpu.ebp, cpu.esp, vaddr_read(cpu.esp, SREG_CS, 4));
-
 	operand_read(&opr_src); // opr_src.val = $ebp的值
 	vaddr_write(cpu.esp, SREG_CS, (data_size / 8), opr_src.val);
 	cpu.esp = cpu.esp - (data_size / 8);
 
 	// 因为暂时不能单步调试, 所以printf打印出来试一下
-	printf("before info reg $ebp=0x%X $esp=0x%X [$esp]=0x%X\n", cpu.ebp, cpu.esp, vaddr_read(cpu.esp + 4, SREG_CS, 4));
+	printf("before info reg $ebp=0x%X $esp=0x%X [$esp]=0x%X\n", cpu.ebp, cpu.esp, vaddr_read(cpu.esp, SREG_CS, 4));
 }
