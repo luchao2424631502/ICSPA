4ad5d4d388296586e4fc87380d8faa260f5dea55
tracer-pa-public <tracer@njuics.org>
2024-04-02 20:31:09 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 20:31:09 up 3 days, 23:00,  7 users,  load average: 0.00, 0.03, 0.00 > unitime > 32e243f794b5cf8be8ae97a56ee15277f6e35a

--- nemu/src/cpu/instr/add.c
+++ nemu/src/cpu/instr/add.c
@@ -14,7 +14,7 @@ static void instr_execute_2op()
 	// 1. 读取$rm 目标寄存器的值
 	operand_read(&opr_dest);
 
-	{printf("\n[ADD] imm=0x%X->register=0x%X \n", opr_src.val, opr_dest.val);}
+	{printf("\n[ADD] imm=0x%X -> old_reg=0x%X \n", opr_src.val, opr_dest.val);}
 
 	// 2. 执行add运算
 	opr_dest.val = alu_add(opr_src.val, opr_dest.val, data_size);
@@ -22,5 +22,5 @@ static void instr_execute_2op()
 	// 2. 值写回到目标寄存器
 	operand_write(&opr_dest);
 
-	{printf("\n[ADD] imm=0x%X->register=0x%X \n", opr_src.val, opr_dest.val);}
+	{printf("\n[ADD] imm=0x%X -> new_reg=0x%X \n", opr_src.val, opr_dest.val);}
 }
