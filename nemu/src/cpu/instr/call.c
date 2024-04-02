5d8c4239336a31d1beae065f82c74be5107ffb82
tracer-pa-public <tracer@njuics.org>
2024-04-02 17:16:27 +0800

    > clean > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 17:16:27 up 3 days, 19:45,  7 users,  load average: 0.00, 0.02, 0.00 > unitime > b3aead9c6d7417492247d354e79cbbcb57b1fb6b

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -2,3 +2,25 @@
 /*
 Put the implementations of `call' instructions here.
 */
+
+// int call_near(uint32_t eip, uint8_t opcode)
+make_instr_func(call_near) {
+	OPERAND rel;
+	rel.type = OPR_IMM;
+	rel.sreg = SREG_CS;
+	rel.data_size = data_size;
+	rel.addr = eip + 1;
+	// 0. 取到call指令跟着的偏移值
+	operand_read(&rel);
+
+	// 1. PUSH RA(下一条指令地址)
+	uint32_t RA = eip + 1 + data_size / 8;
+	cpu.esp -= data_size / 8;
+	vaddr_write(cpu.esp, SREG_CS, (data_size / 8), RA);
+
+	// 2. 跳转到目标地址(下一条指令的地址+偏移量)
+	int offset = sign_ext(rel.val, data_size);
+	cpu.eip += offset;
+
+	return 1 + (data_size / 8);
+}
