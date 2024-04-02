0ab3eef5e81b670ecf8d33be8020af3bd607b590
tracer-pa-public <tracer@njuics.org>
2024-04-02 20:47:49 +0800

    > clean > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 20:47:49 up 3 days, 23:17,  7 users,  load average: 0.00, 0.00, 0.00 > unitime > 9fffeea85313f1eccafbf4d81626fba70a1345

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -19,3 +19,20 @@ make_instr_func(jmp_near)
 
         return 1 + data_size / 8;
 }
+
+// int jmp_b(uint32_t eip, uint8_t opcode)
+make_instr_func(jmp_b)
+{
+	OPERAND rel;
+	rel.type = OPR_IMM;
+	rel.sreg = SREG_CS;
+	rel.data_size = 8; // Jb
+	rel.addr = eip + 1;
+
+	operand_read(&rel);
+
+	int offset = sign_ext(rel.val, 8); // signed-相对偏移
+	cpu.eip += offset;
+	
+	return 1 + 1;
+}
