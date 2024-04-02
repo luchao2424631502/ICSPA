203820189edfbdfd91f98369420d249e6f249733
tracer-pa-public <tracer@njuics.org>
2024-04-02 12:31:17 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 12:31:17 up 3 days, 15:00,  6 users,  load average: 0.04, 0.05, 0.01 > unitime > 76039ffe82595fb578365a5b5c480f8f1dbf8369

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -17,10 +17,11 @@ make_instr_impl_1op(push, r, l) // 生成的push_r_l适用于0x50~0x57, 所以�
 // 每一个宏生成来说, 都给了一个static 用户自己实现的 执行函数
 static void instr_execute_1op()
 {
+	printf("before info reg $esp=0x%X [$old_esp]=0x%X\n", cpu.esp, vaddr_read(cpu.esp, SREG_CS, 4));
 	operand_read(&opr_src); // opr_src.val = $ebp的值
 	vaddr_write(cpu.esp, SREG_CS, (data_size / 8), opr_src.val);
 	cpu.esp = cpu.esp - (data_size / 8);
 
 	// 因为暂时不能单步调试, 所以printf打印出来试一下
-	printf("before info reg $ebp=0x%X $esp=0x%X [$esp]=0x%X\n", cpu.ebp, cpu.esp, vaddr_read(cpu.esp, SREG_CS, 4));
+	printf("before info reg $esp=0x%X [$old_esp]=0x%X [$esp]=0x%X\n", cpu.esp, vaddr_read(cpu.esp + 4, SREG_CS, 4), vaddr_read(cpu.esp, SREG_CS, 4));
 }
