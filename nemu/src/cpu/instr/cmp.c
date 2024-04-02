b6788f780b82c711296a11cc5715e79910601c56
tracer-pa-public <tracer@njuics.org>
2024-04-02 21:41:03 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 21:41:03 up 4 days, 10 min,  7 users,  load average: 0.00, 0.04, 0.01 > unitime > dc5138474b60a9b9b78368eda27d29228ab55229

--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -15,6 +15,10 @@ void instr_execute_2op() // cmp逻辑
 	operand_read(&opr_dest);
 
 	{printf("\n[CMP_BV] imm_val=0x%X reg_val=0x%X\n", opr_src.val, opr_dest.val);}
+	{printf("\teflags [%s %s %s %s]", cpu.eflags.ZF?"ZF":"",
+			cpu.eflags.SF?"SF":"",
+			cpu.eflags.CF?"CF":"",
+			cpu.eflags.OF?"OF":"");}
 	// 2. 执行cmp (实际通过sub指令来影响eflags标志位, 并且执行32bit减法)
 	alu_sub(sign_ext(opr_src.val, 8), opr_dest.val, data_size);
 }
