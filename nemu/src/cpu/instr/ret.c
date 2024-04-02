7f84fbd3751cf7fc99f91a88520d1bbb2485ea5c
tracer-pa-public <tracer@njuics.org>
2024-04-02 19:37:18 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 19:37:18 up 3 days, 22:06,  7 users,  load average: 0.01, 0.01, 0.00 > unitime > 5ed6d13efb174242d6be6033c799d433ea3498c

--- nemu/src/cpu/instr/ret.c
+++ nemu/src/cpu/instr/ret.c
@@ -3,15 +3,19 @@
 Put the implementations of `ret' instructions here.
 */
 /* int ret(uint32_t eip, uint8_t opcode) */
-make_instr_func(ret) {
+make_instr_func(ret) { // ret 直接将栈顶值加载到eip中, 但是我这里没有这么做
+
 	// 0. 取出栈顶绝对地址, 
 	uint32_t dest_addr = vaddr_read(cpu.esp, SREG_CS, 4);
 	
 	// 偏移值 = 目标地址 - 下一条指令地址
 	dest_addr = dest_addr - (eip + 1);
 	int offset = sign_ext(dest_addr, data_size);
+
 	// 1. 修改
 	cpu.eip += offset;
+
+	{printf("\n[RET] offset=%d old_cpu_eip=0x%X new_cpu_eip=0x%X\n", offset, cpu.eip - offset, cpu.eip);}
 
 	return 1;
 }
