231a96000af4e23b31a6f147d206baf240e02ff1
tracer-pa-public <tracer@njuics.org>
2024-04-02 17:20:24 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 17:20:24 up 3 days, 19:49,  7 users,  load average: 0.17, 0.07, 0.01 > unitime > 917e6a2b87945251aed96a466e0ae1d722968d24

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -17,6 +17,8 @@ make_instr_func(call_near) {
 	uint32_t RA = eip + 1 + data_size / 8;
 	cpu.esp -= data_size / 8;
 	vaddr_write(cpu.esp, SREG_CS, (data_size / 8), RA);
+	printf("\n[CALL] old_stack_top=0x%X now_stack_top=0x%X\n", vaddr_read(cpu.esp+4, SREG_CS, 4),
+			vaddr_read(cpu.esp, SREG_CS,4));
 
 	// 2. 跳转到目标地址(下一条指令的地址+偏移量)
 	int offset = sign_ext(rel.val, data_size);
