9c56e51f6cea58da69a6a92f9b1c99c3600d520c
tracer-pa-public <tracer@njuics.org>
2024-04-02 19:34:44 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 19:34:44 up 3 days, 22:04,  7 users,  load average: 0.00, 0.00, 0.00 > unitime > 44063b79406bb5c138b2cc11b7b70d8dfc482fbc

--- nemu/src/cpu/instr/ret.c
+++ nemu/src/cpu/instr/ret.c
@@ -2,3 +2,16 @@
 /*
 Put the implementations of `ret' instructions here.
 */
+/* int ret(uint32_t eip, uint8_t opcode) */
+make_instr_func(ret) {
+	// 0. 取出栈顶绝对地址, 
+	uint32_t dest_addr = vaddr_read(cpu.esp, SREG_CS, 4);
+	
+	// 偏移值 = 目标地址 - 下一条指令地址
+	dest_addr = dest_addr - (eip + 1);
+	int offset = sign_ext(dest_addr, data_size);
+	// 1. 修改
+	cpu.eip += offset;
+
+	return 1;
+}
