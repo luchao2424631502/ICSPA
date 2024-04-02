0fe2c550b2a4f740323761b30bd406ce8411bac6
tracer-pa-public <tracer@njuics.org>
2024-04-02 16:01:13 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 16:01:13 up 3 days, 18:30,  7 users,  load average: 0.11, 0.05, 0.01 > unitime > a3b337d94da1f7a767e137e28b84e5b2897b9038

--- nemu/src/cpu/instr/sub.c
+++ nemu/src/cpu/instr/sub.c
@@ -6,7 +6,7 @@ Put the implementations of `sub' instructions here.
 static void instr_execute_2op();
 
 /* 属于GRP1(Group1)指令组, 该指令组包括多种操作, 其立即数操作大小固定为8bit */
-make_instr_impl_2op(sub, i, rm, b) // sub_i2rm_b
+make_instr_impl_2op(sub, i, rm, bv) // sub_i2rm_bv
 
 static void instr_execute_2op()
 {
