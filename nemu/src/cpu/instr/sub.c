ef5770c47c8c7d95161a1b9999967729ad559af7
tracer-pa-public <tracer@njuics.org>
2024-04-02 16:05:14 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 16:05:14 up 3 days, 18:34,  7 users,  load average: 0.04, 0.05, 0.01 > unitime > 930a91ba56a767a56fc3c0445ca1f3c8473a3b2a

--- nemu/src/cpu/instr/sub.c
+++ nemu/src/cpu/instr/sub.c
@@ -15,12 +15,12 @@ static void instr_execute_2op()
 	// 2. 读取$rm 寄存器的值
 	operand_read(&opr_dest);
 
-	printf("opr_src.val = 0x%X opr_dest.val = 0x%X opr_dest.data_size=%d\n", opr_src.val, opr_dest.val, opr_dest.data_size);
+	printf("opr_src.val=0x%X opr_dest.val=0x%X opr_src.data_size=%d opr_dest.data_size=%d\n", opr_src.val, opr_dest.val, opr_src.data_size, opr_dest.data_size);
 
 	// 3. 执行sub运算
-	opr_dest.val = alu_sub(opr_src.val, opr_dest.val, 32);
+	opr_dest.val = alu_sub(opr_src.val, opr_dest.val, opr_dest.data_size);
 	// 4. 写回到目标寄存器
 	operand_write(&opr_dest);
 	
-	printf("opr_src.val = 0x%X opr_dest.val = 0x%X opr_dest.data_size=%d\n", opr_src.val, opr_dest.val, opr_dest.data_size);
+	printf("opr_src.val=0x%X opr_dest.val=0x%X opr_src.data_size=%d opr_dest.data_size=%d\n", opr_src.val, opr_dest.val, opr_src.data_size, opr_dest.data_size);
 }
