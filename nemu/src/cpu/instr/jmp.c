f3f81adeb938c8d0e4f807844067122698077768
tracer-pa-public <tracer@njuics.org>
2024-04-02 20:55:49 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 20:55:49 up 3 days, 23:25,  7 users,  load average: 0.00, 0.00, 0.00 > unitime > 590aa532b4410ffedbdc4441a876abf919900e

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -32,9 +32,9 @@ make_instr_func(jmp_b)
 
 	operand_read(&rel);
 
-	{printf("\n[JMP_B] old_eip=0x%X new_eip=0x%X \n", eip, eip + 2 + offset);}
 	int offset = sign_ext(rel.val, 8); // signed-相对偏移
 	cpu.eip += offset;
 	
+	{printf("\n[JMP_B] old_eip=0x%X new_eip=0x%X \n", eip, eip + 2 + offset);}
 	return 1 + 1;
 }
