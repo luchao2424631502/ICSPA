88fd2caeacb850113053bd07636f20de777949ad
tracer-pa-public <tracer@njuics.org>
2024-04-03 11:30:15 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 11:30:15 up 4 days, 13:59,  7 users,  load average: 0.05, 0.02, 0.00 > unitime > be39a522e538ed32ce2c514e585751e34d7c59d9

--- nemu/src/cpu/instr/lea.c
+++ nemu/src/cpu/instr/lea.c
@@ -6,4 +6,19 @@ Put the implementations of `lea' instructions here.
 // int lea(uint32_t eip, uint8_t opcode)
 make_instr_func(lea)
 {
+	int len = 1;
+	OPERAND sib, dest;
+	
+	sib.data_size = dest.data_size = data_size;
+
+	// 0. 分析rm sib得到的addr就是lea计算的值
+	len += modrm_r_rm(eip + 1, &dest, &sib);
+
+	// 1. 将lea计算得到的值移动到寄存器
+	dest.val = sib.addr;
+	operand_write(&dest);
+
+	{printf("\n[LEA] ()_val=0x%X -> reg reg_val=0x%X \n", sib.addr, dest.val);}
+
+	return len;
 }
