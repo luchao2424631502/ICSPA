69a9461a0768096b0d0f24b19ede4ff6c214e654
tracer-pa-public <tracer@njuics.org>
2024-04-03 13:19:06 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 13:19:06 up 4 days, 15:48,  7 users,  load average: 0.03, 0.05, 0.00 > unitime > f37fa9d6ec054458dc484ab876469a236409bc9f

--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -16,10 +16,17 @@ void instr_execute_2op() // cmp逻辑
 	operand_read(&opr_dest);
 
 	{printf("\n[CMP_BV] eip=0x%X imm_val=0x%X reg_val=0x%X\n", cpu.eip, opr_src.val, opr_dest.val);}
-	{printf("\teflags before [%s %s %s %s]", cpu.eflags.ZF?"ZF":"",
+	{printf("\teflags before [%s %s %s %s]\n", cpu.eflags.ZF?"ZF":"",
 			cpu.eflags.SF?"SF":"",
 			cpu.eflags.CF?"CF":"",
 			cpu.eflags.OF?"OF":"");}
+	{printf("\t[LEA] eax=0x%X ecx=0x%X edx=0x%X ebx=0x%X esp=0x%X ebp=0x%X\n", 
+			cpu.eax,
+			cpu.ecx,
+			cpu.edx,
+			cpu.ebx,
+			cpu.esp,
+			cpu.ebp);}
 	// 2. 执行cmp (实际通过sub指令来影响eflags标志位, 并且执行32bit减法)
 	alu_sub(sign_ext(opr_src.val, 8), opr_dest.val, data_size);
 
