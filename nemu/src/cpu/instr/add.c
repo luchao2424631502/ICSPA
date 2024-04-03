1cf87a17c75ef89d84edc45568740d2eed55e97c
tracer-pa-public <tracer@njuics.org>
2024-04-03 15:56:49 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 15:56:49 up 4 days, 18:26,  7 users,  load average: 0.00, 0.00, 0.00 > unitime > a8aee44575c729ec150611405b131eb0367fe671

--- nemu/src/cpu/instr/add.c
+++ nemu/src/cpu/instr/add.c
@@ -15,6 +15,15 @@ static void instr_execute_2op()
 	operand_read(&opr_dest);
 
 	{printf("\n[ADD] imm=0x%X -> old_reg=0x%X \n", opr_src.val, opr_dest.val);}
+	{printf("\teax=0x%X ecx=0x%X edx=0x%X ebx=0x%X esp=0x%X ebp=0x%X esi=0x%X edi=0x%X\n",
+			cpu.eax,
+			cpu.ecx,
+			cpu.edx,
+			cpu.ebx,
+			cpu.esp,
+			cpu.ebp,
+			cpu.esi,
+			cpu.edi);}
 
 	// 2. 执行add运算
 	opr_dest.val = alu_add(opr_src.val, opr_dest.val, data_size);
@@ -22,5 +31,14 @@ static void instr_execute_2op()
 	// 2. 值写回到目标寄存器
 	operand_write(&opr_dest);
 
+	{printf("\teax=0x%X ecx=0x%X edx=0x%X ebx=0x%X esp=0x%X ebp=0x%X esi=0x%X edi=0x%X\n",
+			cpu.eax,
+			cpu.ecx,
+			cpu.edx,
+			cpu.ebx,
+			cpu.esp,
+			cpu.ebp,
+			cpu.esi,
+			cpu.edi);}
 	{printf("\timm=0x%X -> new_reg=0x%X ebp=0x%X esp=0x%X \n", opr_src.val, opr_dest.val, cpu.ebp, cpu.esp);}
 }
