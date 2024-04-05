046ad202607e8fb29d1225e796fb26b6e416ec3d
tracer-pa-public <tracer@njuics.org>
2024-04-05 16:05:42 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 16:05:42 up 6 days, 18:35,  7 users,  load average: 0.26, 0.08, 0.02 > unitime > 5640adf013526ca7bc4f4503c75080a5cd5f125

--- nemu/src/cpu/instr/leave.c
+++ nemu/src/cpu/instr/leave.c
@@ -4,11 +4,30 @@ Put the implementations of `leave' instructions here.
 */
 make_instr_func(leave)
 {
+	printf("\n[LEAVE]\n");
+	{printf("\teax=0x%X ecx=0x%X edx=0x%X ebx=0x%X esp=0x%X ebp=0x%X esi=0x%X edi=0x%X\n",
+			cpu.eax,
+			cpu.ecx,
+			cpu.edx,
+			cpu.ebx,
+			cpu.esp,
+			cpu.ebp,
+			cpu.esi,
+			cpu.edi);}
 	// 0. 释放栈帧 将esp恢复为栈基址ebp
 	cpu.esp = cpu.ebp;
 
 	// 1. 将栈机制恢复为调用者的栈基址 pop %ebp
 	cpu.ebp = vaddr_read(cpu.esp, SREG_CS, data_size / 8);
 	cpu.esp += data_size / 8;
+	{printf("\teax=0x%X ecx=0x%X edx=0x%X ebx=0x%X esp=0x%X ebp=0x%X esi=0x%X edi=0x%X\n",
+			cpu.eax,
+			cpu.ecx,
+			cpu.edx,
+			cpu.ebx,
+			cpu.esp,
+			cpu.ebp,
+			cpu.esi,
+			cpu.edi);}
 	return 1;
 }
