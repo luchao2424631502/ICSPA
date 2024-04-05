207ba403d07fceaf45997fa5bc9461690f46bb7a
tracer-pa-public <tracer@njuics.org>
2024-04-05 09:56:14 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 09:56:14 up 6 days, 12:25,  7 users,  load average: 0.24, 0.10, 0.02 > unitime > 628e4672b42af8b0d37e7179e9dc721ab9b8cfea

--- nemu/src/cpu/instr/and.c
+++ nemu/src/cpu/instr/and.c
@@ -24,6 +24,9 @@ static void instr_execute_2op()
 			cpu.ebp);}
 	// 2. and运算
 	opr_dest.val = alu_and(sign_ext(opr_src.val, opr_dest.data_size), opr_dest.val, opr_dest.data_size);
+	// 3. 写回
+	operand_write(&opr_dest);
+
 	{printf("\teax=0x%X ecx=0x%X edx=0x%X ebx=0x%X esp=0x%X ebp=0x%X\n", 
 			cpu.eax,
 			cpu.ecx,
@@ -32,6 +35,4 @@ static void instr_execute_2op()
 			cpu.esp,
 			cpu.ebp);}
 	
-	// 3. 写回
-	operand_write(&opr_dest);
 }
