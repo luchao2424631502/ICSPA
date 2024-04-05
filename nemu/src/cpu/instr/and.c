88d7d5c91b97070d47bd448e4484985f7ee31a82
tracer-pa-public <tracer@njuics.org>
2024-04-05 15:05:57 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 15:05:57 up 6 days, 17:35,  7 users,  load average: 0.13, 0.06, 0.01 > unitime > 3e6974f1988edf93866d9fef181c7fe0a68b6f37

--- nemu/src/cpu/instr/and.c
+++ nemu/src/cpu/instr/and.c
@@ -5,6 +5,7 @@ Put the implementations of `and' instructions here.
 static void instr_execute_2op();
 
 make_instr_impl_2op(and, i, rm, bv)	// and_i2rm_bv
+make_instr_impl_2op(and, rm, r, b)	// and_rm2r_b
 
 static void instr_execute_2op()
 {
