24dd7473cf74e42498463f81edccf0fb7aad601a
tracer-pa-public <tracer@njuics.org>
2024-04-05 11:20:48 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 11:20:48 up 6 days, 13:50,  7 users,  load average: 0.05, 0.03, 0.00 > unitime > bc578202e41618dffbbfb038491385244b9e7fb

--- nemu/include/cpu/instr/adc.h
+++ nemu/include/cpu/instr/adc.h
@@ -3,5 +3,6 @@
 /*
 Put the declarations of `adc' instructions here.
 */
+make_instr_func(adc_r2rm_v);
 
 #endif
--- nemu/src/cpu/instr/adc.c
+++ nemu/src/cpu/instr/adc.c
@@ -2,3 +2,40 @@
 /*
 Put the implementations of `adc' instructions here.
 */
+static void instr_execute_2op();
+
+make_instr_impl_2op(adc, r, rm, v); // adc_r2rm_v
+
+static void instr_execute_2op()
+{
+	// 0. 读取源操作数
+	operand_read(&opr_src);
+	// 1. 读取目的操作数
+	operand_read(&opr_dest);
+
+	{printf("\n[ADC] imm=0x%X -> old_reg=0x%X \n", opr_src.val, opr_dest.val);}
+	{printf("\teax=0x%X ecx=0x%X edx=0x%X ebx=0x%X esp=0x%X ebp=0x%X esi=0x%X edi=0x%X\n",
+			cpu.eax,
+			cpu.ecx,
+			cpu.edx,
+			cpu.ebx,
+			cpu.esp,
+			cpu.ebp,
+			cpu.esi,
+			cpu.edi);}
+	// 2. adc
+	opr_dest.val = alu_adc(opr_src.val, opr_dest.val, data_size);
+
+	// 3. 写回
+	operand_write(&opr_dest);
+	{printf("\teax=0x%X ecx=0x%X edx=0x%X ebx=0x%X esp=0x%X ebp=0x%X esi=0x%X edi=0x%X\n",
+			cpu.eax,
+			cpu.ecx,
+			cpu.edx,
+			cpu.ebx,
+			cpu.esp,
+			cpu.ebp,
+			cpu.esi,
+			cpu.edi);}
+	{printf("\timm=0x%X -> new_reg=0x%X ebp=0x%X esp=0x%X \n", opr_src.val, opr_dest.val, cpu.ebp, cpu.esp);}
+}
