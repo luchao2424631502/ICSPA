03376bb9b905ac3b2d88add0984921527e39a813
tracer-pa-public <tracer@njuics.org>
2024-04-05 13:33:58 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 13:33:58 up 6 days, 16:03,  7 users,  load average: 0.02, 0.03, 0.00 > unitime > 17db8c219b8efd634e2cf814471bfcda9ead8976

--- nemu/src/cpu/instr/test.c
+++ nemu/src/cpu/instr/test.c
@@ -2,3 +2,27 @@
 /*
 Put the implementations of `test' instructions here.
 */
+static void instr_execute_2op();
+
+make_instr_impl_2op(test, r, rm, v); // test_r2rm_v
+
+static void instr_execute_2op()
+{
+	// 0. 读取源
+	operand_read(&opr_src);
+	// 1. 读取目的
+	operand_read(&opr_dest);
+	// 2. 执行and操作, 结果丢弃, 只影响标志位即可
+	alu_add(opr_src.val, opr_dest.val, data_size);
+
+	{printf("\n[TEST]\n");}
+	{printf("\teax=0x%X ecx=0x%X edx=0x%X ebx=0x%X esp=0x%X ebp=0x%X esi=0x%X edi=0x%X\n", 
+			cpu.eax,
+			cpu.ecx,
+			cpu.edx,
+			cpu.ebx,
+			cpu.esp,
+			cpu.ebp,
+			cpu.esi,
+			cpu.edi);}
+}
