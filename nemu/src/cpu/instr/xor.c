320516ef9345f23b5aba2fe970b62ba8877d746e
tracer-pa-public <tracer@njuics.org>
2024-04-05 13:13:22 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 13:13:22 up 6 days, 15:42,  7 users,  load average: 0.06, 0.04, 0.00 > unitime > b7a37e4762255501de9d889273add8648477b1c7

--- nemu/src/cpu/instr/xor.c
+++ nemu/src/cpu/instr/xor.c
@@ -9,13 +9,15 @@ make_instr_impl_2op(xor, r, rm, v); // xor_r2rm_v
 static void instr_execute_2op()
 {
 	{printf("\n[XOR]\n");}
-	{printf("\teax=0x%X ecx=0x%X edx=0x%X ebx=0x%X esp=0x%X ebp=0x%X\n", 
+	{printf("\teax=0x%X ecx=0x%X edx=0x%X ebx=0x%X esp=0x%X ebp=0x%X esi=0x%X edi=0x%X\n", 
 			cpu.eax,
 			cpu.ecx,
 			cpu.edx,
 			cpu.ebx,
 			cpu.esp,
-			cpu.ebp);}
+			cpu.ebp,
+			cpu.esi,
+			cpu.edi);}
 	// 0. 读取源操作数
 	operand_read(&opr_src);
 	// 1. 读取目的操作数
@@ -24,11 +26,13 @@ static void instr_execute_2op()
 	opr_dest.val = alu_xor(opr_src.val, opr_dest.val, data_size);
 	// 3. 写回
 	operand_write(&opr_dest);
-	{printf("\teax=0x%X ecx=0x%X edx=0x%X ebx=0x%X esp=0x%X ebp=0x%X\n", 
+	{printf("\teax=0x%X ecx=0x%X edx=0x%X ebx=0x%X esp=0x%X ebp=0x%X esi=0x%X edi=0x%X\n", 
 			cpu.eax,
 			cpu.ecx,
 			cpu.edx,
 			cpu.ebx,
 			cpu.esp,
-			cpu.ebp);}
+			cpu.ebp,
+			cpu.esi,
+			cpu.edi);}
 }
