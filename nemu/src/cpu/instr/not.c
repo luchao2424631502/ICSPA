e1bc47ddce560d615c4fe917f596d92d7133f2e9
tracer-pa-public <tracer@njuics.org>
2024-04-05 17:06:51 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 17:06:51 up 6 days, 19:36,  7 users,  load average: 0.06, 0.02, 0.00 > unitime > d96388ce11038fe3341de4d8f9f0f544341a73cf

--- nemu/src/cpu/instr/not.c
+++ nemu/src/cpu/instr/not.c
@@ -2,3 +2,23 @@
 /*
 Put the implementations of `not' instructions here.
 */
+static void instr_execute_1op();
+
+make_instr_impl_1op(not, rm, v) // not_rm_v
+
+static void instr_execute_1op()
+{
+	// 0. 读取源操作数 
+	operand_read(&opr_src);
+	
+	// 1. not 运算
+	opr_src.val = ~opr_src.val;
+	
+	if (opr_src.data_size == 8) 
+		opr_src.val = 0xFF & opr_src.val;
+	else if (opr_src.data_size == 16)
+		opr_src.val = 0xFFFF & opr_src.val;
+
+	// 2. 写回
+	operand_write(&opr_src);
+}
