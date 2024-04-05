64335a9cd89df337bf2e6dd78b55740716b3d303
tracer-pa-public <tracer@njuics.org>
2024-04-05 11:22:03 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 11:22:03 up 6 days, 13:51,  7 users,  load average: 0.07, 0.04, 0.00 > unitime > 90bba907e441775274a4da17e54b43562d01ef9a

--- nemu/src/cpu/instr/adc.c
+++ nemu/src/cpu/instr/adc.c
@@ -8,13 +8,13 @@ make_instr_impl_2op(adc, r, rm, v); // adc_r2rm_v
 
 static void instr_execute_2op()
 {
+	cpu.ebx = 0x9999;
+	cpu.edx = 0x1;
+
 	// 0. 读取源操作数
 	operand_read(&opr_src);
 	// 1. 读取目的操作数
 	operand_read(&opr_dest);
-
-	cpu.ebx = 0x9999;
-	cpu.edx = 0x1;
 
 	{printf("\n[ADC] imm=0x%X -> old_reg=0x%X \n", opr_src.val, opr_dest.val);}
 	{printf("\teax=0x%X ecx=0x%X edx=0x%X ebx=0x%X esp=0x%X ebp=0x%X esi=0x%X edi=0x%X\n",
