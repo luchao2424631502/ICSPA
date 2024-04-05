dd653d439d78f7742279e9b4dc976d5e8626c43a
tracer-pa-public <tracer@njuics.org>
2024-04-05 17:09:01 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 17:09:01 up 6 days, 19:38,  7 users,  load average: 0.00, 0.00, 0.00 > unitime > 69a185c3324c3405ba645d345eb6da5b5a502747

--- nemu/src/cpu/instr/not.c
+++ nemu/src/cpu/instr/not.c
@@ -8,6 +8,16 @@ make_instr_impl_1op(not, rm, v) // not_rm_v
 
 static void instr_execute_1op()
 {
+	{printf("\n[NOT]\n");}
+	{printf("\teax=0x%X ecx=0x%X edx=0x%X ebx=0x%X esp=0x%X ebp=0x%X esi=0x%X edi=0x%X\n", 
+			cpu.eax,
+			cpu.ecx,
+			cpu.edx,
+			cpu.ebx,
+			cpu.esp,
+			cpu.ebp,
+			cpu.esi,
+			cpu.edi);}
 	// 0. 读取源操作数 
 	operand_read(&opr_src);
 	
@@ -21,4 +31,13 @@ static void instr_execute_1op()
 
 	// 2. 写回
 	operand_write(&opr_src);
+	{printf("\teax=0x%X ecx=0x%X edx=0x%X ebx=0x%X esp=0x%X ebp=0x%X esi=0x%X edi=0x%X\n", 
+			cpu.eax,
+			cpu.ecx,
+			cpu.edx,
+			cpu.ebx,
+			cpu.esp,
+			cpu.ebp,
+			cpu.esi,
+			cpu.edi);}
 }
