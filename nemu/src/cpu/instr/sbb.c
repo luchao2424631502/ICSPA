17d336883f2cf7b2dff984306db1168faa2934e8
tracer-pa-public <tracer@njuics.org>
2024-04-05 19:39:28 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 19:39:28 up 6 days, 22:08,  7 users,  load average: 0.09, 0.03, 0.01 > unitime > 4b21685b8760e6ce3de923ad22f0ced2470b81fa

--- nemu/src/cpu/instr/sbb.c
+++ nemu/src/cpu/instr/sbb.c
@@ -2,3 +2,39 @@
 /*
 Put the implementations of `sbb' instructions here.
 */
+static void instr_execute_2op();
+
+make_instr_impl_2op(sbb, rm, r, v) // sbb_rm2r_v
+
+static void instr_execute_2op()
+{
+	// 0. 取出源操作数
+	operand_read(&opr_src);
+	// 1.
+	operand_read(&opr_dest);
+
+	printf("\n[SBB]_before opr_src.val=0x%X opr_dest.val=0x%X opr_src.data_size=%d opr_dest.data_size=%d\n", opr_src.val, opr_dest.val, opr_src.data_size, opr_dest.data_size);
+	{printf("\teax=0x%X ecx=0x%X edx=0x%X ebx=0x%X esp=0x%X ebp=0x%X esi=0x%X edi=0x%X\n",
+			cpu.eax,
+			cpu.ecx,
+			cpu.edx,
+			cpu.ebx,
+			cpu.esp,
+			cpu.ebp,
+			cpu.esi,
+			cpu.edi);}
+	// 2.
+	opr_dest.val = alu_sbb(opr_src.val, opr_dest.val, opr_dest.data_size);
+
+	// 3. 写回
+	operand_write(&opr_dest);
+	{printf("\teax=0x%X ecx=0x%X edx=0x%X ebx=0x%X esp=0x%X ebp=0x%X esi=0x%X edi=0x%X\n",
+			cpu.eax,
+			cpu.ecx,
+			cpu.edx,
+			cpu.ebx,
+			cpu.esp,
+			cpu.ebp,
+			cpu.esi,
+			cpu.edi);}
+}
