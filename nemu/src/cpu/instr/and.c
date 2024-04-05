e109f503b845b15ff99f438e48af0dc7290a6bf2
tracer-pa-public <tracer@njuics.org>
2024-04-05 09:29:42 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 09:29:42 up 6 days, 11:59,  7 users,  load average: 0.04, 0.05, 0.01 > unitime > 71f74bdf3e47fadfc252fc9996f6cf35241e48ed

--- nemu/src/cpu/instr/and.c
+++ nemu/src/cpu/instr/and.c
@@ -14,8 +14,22 @@ static void instr_execute_2op()
 	// 1. 读取立即数的值
 	operand_read(&opr_src);
 
+	{printf("\teax=0x%X ecx=0x%X edx=0x%X ebx=0x%X esp=0x%X ebp=0x%X\n", 
+			cpu.eax,
+			cpu.ecx,
+			cpu.edx,
+			cpu.ebx,
+			cpu.esp,
+			cpu.ebp);}
 	// 2. and运算
 	opr_dest.val = alu_and(sign_ext(opr_src.val, opr_dest.data_size), opr_dest.val, opr_dest.data_size);
+	{printf("\teax=0x%X ecx=0x%X edx=0x%X ebx=0x%X esp=0x%X ebp=0x%X\n", 
+			cpu.eax,
+			cpu.ecx,
+			cpu.edx,
+			cpu.ebx,
+			cpu.esp,
+			cpu.ebp);}
 	
 	// 3. 写回
 	operand_write(&opr_dest);
