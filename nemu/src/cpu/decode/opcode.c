f1674c6f1baeb4752ba9017b49ff05786b86d5f1
tracer-pa-public <tracer@njuics.org>
2024-04-05 11:44:09 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 11:44:09 up 6 days, 14:13,  7 users,  load average: 0.06, 0.06, 0.01 > unitime > 847830289071eb97357f183254e339931eaa551e

--- nemu/src/cpu/decode/opcode.c
+++ nemu/src/cpu/decode/opcode.c
@@ -23,8 +23,8 @@ instr_func opcode_entry[256] = {
     /* 0x4c - 0x4f*/ inv, inv, inv, inv,
     /* 0x50 - 0x53*/ push_r_l, push_r_l, push_r_l, push_r_l, /* 24.4.2 添加push(2)硬编码指令 */
     /* 0x54 - 0x57*/ push_r_l, push_r_l, push_r_l, push_r_l, 
-    /* 0x58 - 0x5b*/ inv, inv, inv, inv,
-    /* 0x5c - 0x5f*/ inv, inv, inv, inv,
+    /* 0x58 - 0x5b*/ pop_r_v, pop_r_v, pop_r_v, pop_r_v, /* 24.4.5 添加 pop(2_8) 指令 */
+    /* 0x5c - 0x5f*/ pop_r_v, pop_r_v, pop_r_v, pop_r_v,
     /* 0x60 - 0x63*/ inv, inv, inv, inv,
     /* 0x64 - 0x67*/ inv, inv, data_size_16, inv,
     /* 0x68 - 0x6b*/ inv, inv, inv, inv,
--- nemu/src/cpu/instr/pop.c
+++ nemu/src/cpu/instr/pop.c
@@ -2,3 +2,37 @@
 /*
 Put the implementations of `pop' instructions here.
 */
+static void instr_execute_1op();
+
+make_instr_impl_1op(pop, r, v) // pop_r_v v对应data_size l对应32
+
+static void instr_execute_1op()
+{
+	printf("\n[POP]\n");
+	{printf("\teax=0x%X ecx=0x%X edx=0x%X ebx=0x%X esp=0x%X ebp=0x%X esi=0x%X edi=0x%X\n",
+			cpu.eax,
+			cpu.ecx,
+			cpu.edx,
+			cpu.ebx,
+			cpu.esp,
+			cpu.ebp,
+			cpu.esi,
+			cpu.edi);}
+	{printf("\t current_top_of_stack_value=0x%X", vaddr_read(cpu.esp, SREG_CS, data_size));}
+	// 0. 取出当前栈顶的值
+	uint32_t top_val = vaddr_read(cpu.esp, SREG_CS, data_size);
+	cpu.esp += data_size / 8; 
+	
+	// 1. 将栈顶的值写回到指定寄存器
+	opr_src.val = top_val;
+	operand_write(&opr_src);
+	{printf("\teax=0x%X ecx=0x%X edx=0x%X ebx=0x%X esp=0x%X ebp=0x%X esi=0x%X edi=0x%X\n",
+			cpu.eax,
+			cpu.ecx,
+			cpu.edx,
+			cpu.ebx,
+			cpu.esp,
+			cpu.ebp,
+			cpu.esi,
+			cpu.edi);}
+}
