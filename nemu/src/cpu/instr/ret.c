10fb75ab07045342e823f8418c1fee0ccbfb0f8a
tracer-pa-public <tracer@njuics.org>
2024-04-06 14:21:46 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 14:21:46 up 7 days, 16:51,  7 users,  load average: 1.50, 1.43, 0.86 > unitime > 5cc982a16dfbae8d6a93a39498f666db24244d83

--- nemu/src/cpu/instr/ret.c
+++ nemu/src/cpu/instr/ret.c
@@ -30,24 +30,18 @@ make_instr_func(ret)
 
 make_instr_func(ret_imm16)
 {
-	int len = 1;
-	// 0. 读Imm
-	uint16_t imm = vaddr_read(eip + 1, SREG_CS, 2);
-	len += 2;
-	// 1. 
+	// 0. 直接修改eip
 	uint32_t dest_addr = vaddr_read(cpu.esp, SREG_CS, 4);
 	cpu.esp += data_size / 8;
 
-	// 这里计算地址出错了
-	dest_addr = dest_addr - (eip + len); // 还是计算偏移地址加到eip上, 因为exec_instr()执行完后会+len, 所以不好直接
-	int offset = sign_ext(dest_addr, data_size);
-
-	cpu.eip += offset;
-
-	// 2. pop imm16 bytes
+	// 1. 直接修改eip
+	cpu.eip = dest_addr;
+	
+	// 2. 释放栈参数
+	uint16_t imm = vaddr_read(eip + 1, SREG_CS, 2);
 	cpu.esp += imm;
 
-	{printf("\n[RET_imm16] offset=%d old_cpu_eip=0x%X new_cpu_eip=0x%X imm=%u\n", offset, eip, eip + offset + len, imm);}
+	{printf("\n[RET_IMM16] old_cpu_eip=0x%X new_cpu_eip=0x%X\n", eip, cpu.eip);}
 	{printf("\teax=0x%X ecx=0x%X edx=0x%X ebx=0x%X esp=0x%X ebp=0x%X esi=0x%X edi=0x%X\n",
 			cpu.eax,
 			cpu.ecx,
@@ -58,6 +52,6 @@ make_instr_func(ret_imm16)
 			cpu.esi,
 			cpu.edi);}
 	printf("\t curr_stack_top_value=0x%X\n", vaddr_read(cpu.esp , SREG_CS, 4));
-
-	return len;
+	
+	return 0;
 }
