1ffb5c89e5b769b62b57db4d77108ba312759887
tracer-pa-public <tracer@njuics.org>
2024-04-07 16:14:25 +0800

    > clean > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 16:14:25 up 8 days, 18:43,  7 users,  load average: 0.00, 0.02, 0.00 > unitime > ebac829ee9c66381c38e9ad8d3d9d394fb42a28

--- nemu/src/cpu/instr/cmov.c
+++ nemu/src/cpu/instr/cmov.c
@@ -12,7 +12,7 @@ static void instr_execute_2op_cc_fail() { /*Do nothing*/}
 make_instr_impl_2op_cc(cmov, rm, r, v, a)
 make_instr_impl_2op_cc(cmov, rm, r, v, ae)
 make_instr_impl_2op_cc(cmov, rm, r, v, b)
-make_instr_impl_2op_cc(cmov, rm, r, v, be)
+make_instr_impl_2op_cc(cmov, rm, r, v, be) // cmovbe_rm2r_v
 make_instr_impl_2op_cc(cmov, rm, r, v, g) // cmovg_rm2r_v
 make_instr_impl_2op_cc(cmov, rm, r, v, ge)
 make_instr_impl_2op_cc(cmov, rm, r, v, l)
