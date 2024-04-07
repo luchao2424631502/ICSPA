8feb61bd5a69f8216caabbdbcbbf369102c09c3b
tracer-pa-public <tracer@njuics.org>
2024-04-07 18:47:19 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 18:47:19 up 8 days, 21:16,  8 users,  load average: 0.02, 0.06, 0.02 > unitime > 85ec7c4f61263936be02da89d9e4abb80bf1a93

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -17,7 +17,7 @@ make_instr_func(call_near) {
 	uint32_t RA = eip + 1 + data_size / 8;
 	cpu.esp -= data_size / 8;
 	vaddr_write(cpu.esp, SREG_CS, (data_size / 8), RA);
-	printf("\n[CALL] old_stack_top=0x%X RA(now_stack_top)=0x%X\n", vaddr_read(cpu.esp+4, SREG_CS, 4),
+	printf("\n[CALL] next_eip=0x%X old_stack_top=0x%X RA(now_stack_top)=0x%X\n", cpu.eip+sign_ext(rel.val, data_size) , vaddr_read(cpu.esp+4, SREG_CS, 4),
 			vaddr_read(cpu.esp, SREG_CS,4));
 
 	{printf("\teax=0x%X ecx=0x%X edx=0x%X ebx=0x%X esp=0x%X ebp=0x%X esi=0x%X edi=0x%X\n",
