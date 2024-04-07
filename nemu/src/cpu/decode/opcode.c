5a4f5c309857855e1bbcaaf3864ceef74f7323cd
tracer-pa-public <tracer@njuics.org>
2024-04-07 12:44:22 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 12:44:22 up 8 days, 15:13,  8 users,  load average: 0.00, 0.02, 0.02 > unitime > 9b79a0878b41d5bddb32e77f643acb8830801fe

--- nemu/src/cpu/decode/opcode.c
+++ nemu/src/cpu/decode/opcode.c
@@ -41,7 +41,7 @@ instr_func opcode_entry[256] = {
     /* 0x94 - 0x97*/ inv, inv, inv, inv,
     /* 0x98 - 0x9b*/ cbw_a_v, cltd, inv, inv,
     /* 0x9c - 0x9f*/ inv, inv, sahf, inv,
-    /* 0xa0 - 0xa3*/ mov_o2a_b, mov_o2a_v, mov_a2o_b, mov_a3o_v,
+    /* 0xa0 - 0xa3*/ mov_o2a_b, mov_o2a_v, mov_a2o_b, mov_a2o_v,
     /* 0xa4 - 0xa7*/ movs_b, movs_v, cmps_b, cmps_v,
     /* 0xa8 - 0xab*/ test_i2a_b, test_i2a_v, stos_b, inv,
     /* 0xac - 0xaf*/ inv, inv, inv, inv,
