44739cc99d515f0f5c60e88d6f197ebec7fbb34f
tracer-pa-public <tracer@njuics.org>
2024-04-07 19:40:20 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 19:40:20 up 8 days, 22:09,  9 users,  load average: 0.07, 0.04, 0.01 > unitime > 4ce0bd4141cccac1e731454a99ff19cef9bc6bfa

--- nemu/include/cpu/instr/sub.h
+++ nemu/include/cpu/instr/sub.h
@@ -8,5 +8,6 @@ make_instr_func(sub_i2rm_bv);
 make_instr_func(sub_rm2r_v);
 make_instr_func(sub_r2rm_v);
 make_instr_func(sub_i2rm_v);
+make_instr_func(sub_i2a_b);
 
 #endif
--- nemu/src/cpu/instr/sub.c
+++ nemu/src/cpu/instr/sub.c
@@ -10,6 +10,7 @@ make_instr_impl_2op(sub, i, rm, bv) // sub_i2rm_bv
 make_instr_impl_2op(sub, rm, r, v) // sub_rm2r_v
 make_instr_impl_2op(sub, r, rm, v) // sub_r2rm_v
 make_instr_impl_2op(sub, i, rm, v) // sub_i2rm_v
+make_instr_impl_2op(sub, i,  a, b) // sub_i2a_b
 
 static void instr_execute_2op()
 {
