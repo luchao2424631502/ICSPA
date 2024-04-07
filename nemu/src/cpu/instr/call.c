120cadc0f80d3713ed9a55002e5d8de9c28af2e6
tracer-pa-public <tracer@njuics.org>
2024-04-07 18:50:55 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 18:50:55 up 8 days, 21:20,  8 users,  load average: 0.03, 0.05, 0.01 > unitime > a30655188c5f2177abf742dc5d1e579e526bda

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -17,21 +17,14 @@ make_instr_func(call_near) {
 	uint32_t RA = eip + 1 + data_size / 8;
 	cpu.esp -= data_size / 8;
 	vaddr_write(cpu.esp, SREG_CS, (data_size / 8), RA);
-	printf("\n[CALL] next_eip=0x%X old_stack_top=0x%X RA(now_stack_top)=0x%X\n", cpu.eip+sign_ext(rel.val, data_size) , vaddr_read(cpu.esp+4, SREG_CS, 4),
+	printf("\n[CALL] cur_eip=0x%X next_eip=0x%X old_stack_top=0x%X RA(now_stack_top)=0x%X\n", cpu.eip, cpu.eip+sign_ext(rel.val, data_size) , vaddr_read(cpu.esp+4, SREG_CS, 4),
 			vaddr_read(cpu.esp, SREG_CS,4));
 
-	{printf("\teax=0x%X ecx=0x%X edx=0x%X ebx=0x%X esp=0x%X ebp=0x%X esi=0x%X edi=0x%X\n",
-			cpu.eax,
-			cpu.ecx,
-			cpu.edx,
-			cpu.ebx,
-			cpu.esp,
-			cpu.ebp,
-			cpu.esi,
-			cpu.edi);}
 	// 2. 跳转到目标地址(下一条指令的地址+偏移量)
 	int offset = sign_ext(rel.val, data_size);
 	cpu.eip += offset;
+
+	{printf("\t offset=0x%X\n", offset);}
 
 	return 1 + data_size / 8;
 }
