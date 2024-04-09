449a673b93d27a20967a9f21d05e96fe6e70ead5
tracer-pa-public <tracer@njuics.org>
2024-04-09 12:07:05 +0800

    > clean > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 12:07:05 up  2:39,  5 users,  load average: 0.01, 0.03, 0.08 > unitime > c3bfd07e55dcb57f7d7d649b848064bc3fc5b15

--- nemu/src/monitor/expr.c
+++ nemu/src/monitor/expr.c
@@ -88,7 +88,7 @@ static bool make_token(char *e)
 		/* Try all rules one by one. */
 		for (i = 0; i < NR_REGEX; i++)
 		{
-			printf("re[i]=%s", re[i]);
+			printf("re[i]=%s", e + position);
 			if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0)
 			{
 				char *substr_start = e + position;
