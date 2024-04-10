e41f372a0399effd1f47898bc0990ccca0dd4eb7
tracer-pa-public <tracer@njuics.org>
2024-04-10 15:32:35 +0800

    > clean > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 15:32:35 up 1 day,  6:04,  5 users,  load average: 0.17, 0.06, 0.04 > unitime > 915dda278440b9361c66c43464699f85e23c0c99

--- nemu/src/monitor/expr.c
+++ nemu/src/monitor/expr.c
@@ -299,7 +299,7 @@ static uint32_t varobject_addr(Elf32_Ehdr *elf, char *varname)
 {
 	tab_desc symtab = nametab_base(elf, ".symtab");
 	tab_desc strtab = nametab_base(elf, ".strtab");
-	// printf("PPP symtab.base=%x strtab.base=0x%x\n", HEXADDR(symtab.base), HEXADDR(strtab.base));
+	printf("PPP symtab.base=%x strtab.base=0x%x\n", HEXADDR(symtab.base), HEXADDR(strtab.base));
 	char *strtab_base = strtab.base;
 	Elf32_Sym *entry = symtab.base;
 	for (int i = 0; i < symtab.num; i++) {
