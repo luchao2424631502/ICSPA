0b9b674b5fb4736031140c953e7350d9a0b74cf4
tracer-pa-public <tracer@njuics.org>
2024-04-10 10:04:18 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 10:04:18 up 1 day, 36 min,  5 users,  load average: 0.07, 0.06, 0.01 > unitime > 52eea3ea2053e2eec1acacdd71bacfc2f118eebc

--- nemu/src/monitor/expr.c
+++ nemu/src/monitor/expr.c
@@ -157,7 +157,7 @@ static bool make_token(char *e)
 
 	// 打印 token 数组
 	for (int i = 0; i < nr_token; i++) {
-		if (tokens[i].type == NUM)
+		if (tokens[i].type == NUM || tokens[i].type == SYMB)
 			printf("%s", tokens[i].str);
 		else	
 			printf("%c", tokens[i].type);
