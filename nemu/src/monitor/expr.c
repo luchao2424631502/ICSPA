0e43d61da7f948429eeb62e2d0a96af7dacd2528
tracer-pa-public <tracer@njuics.org>
2024-04-10 15:32:15 +0800

    > clean > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 15:32:15 up 1 day,  6:04,  5 users,  load average: 0.15, 0.05, 0.03 > unitime > 1517773fb9519de74cd690ee5d0d07e5b53837d

--- nemu/src/monitor/expr.c
+++ nemu/src/monitor/expr.c
@@ -299,13 +299,14 @@ static uint32_t varobject_addr(Elf32_Ehdr *elf, char *varname)
 {
 	tab_desc symtab = nametab_base(elf, ".symtab");
 	tab_desc strtab = nametab_base(elf, ".strtab");
-	printf("PPP symtab.base=%x strtab.base=0x%x", HEXADDR(symtab.base), HEXADDR(strtab.base));
-	return 0;
+	// printf("PPP symtab.base=%x strtab.base=0x%x\n", HEXADDR(symtab.base), HEXADDR(strtab.base));
 	char *strtab_base = strtab.base;
 	Elf32_Sym *entry = symtab.base;
 	for (int i = 0; i < symtab.num; i++) {
-		if (0 == strcmp(strtab_base + (entry + i)->st_name, varname)) {
-			return (entry + i)->st_value;
+		// if (0 == strcmp(strtab_base + (entry + i)->st_name, varname)) {
+		if (0 == strcmp(strtab_base + vaddr_read(HEXADDR(&((entry + i)->st_name)) , SREG_CS, 4), varname)) {
+			// return (entry + i)->st_value;
+			return vaddr_read(HEXADDR(&((entry + i)->st_name)) , SREG_CS, 4);
 		}
 	}
 	return 0;
