99d59ffac4a53647e18446de30bcefd8c3d9ba38
tracer-pa-public <tracer@njuics.org>
2024-04-11 17:25:35 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 17:25:35 up 2 days,  7:57,  6 users,  load average: 0.03, 0.06, 0.01 > unitime > 6a789d57f2da67c7659f4574df2116866974f2f3

--- nemu/src/memory/mmu/cache.c
+++ nemu/src/memory/mmu/cache.c
@@ -60,7 +60,8 @@ uint32_t cache_read(paddr_t paddr, size_t len)
 	uint32_t tmp = is_span(paddr, len);
 	if (tmp) {
 		uint32_t ret1 = cache_read(paddr, tmp);
-		uint32_t ret2 = cache_read(((void *)paddr + tmp), len - tmp);
+		paddr = (paddr_t)((char *)paddr + tmp);
+		uint32_t ret2 = cache_read(paddr, len - tmp);
 		return ret1 | (ret2 << (8 * tmp));
 		printf("[%s] span cache_line read\n", __func__);
 		assert(0);
