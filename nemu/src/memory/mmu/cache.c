78596d08405b91353d54c3ad067f342be426e9da
tracer-pa-public <tracer@njuics.org>
2024-04-11 21:16:00 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 21:16:00 up 2 days, 11:48,  8 users,  load average: 0.03, 0.05, 0.06 > unitime > 9f8a55a28dd9d6258dc39c2ad8ce8915cc984198

--- nemu/src/memory/mmu/cache.c
+++ nemu/src/memory/mmu/cache.c
@@ -54,6 +54,8 @@ void cache_write(paddr_t paddr, size_t len, uint32_t data)
 	// implement me in PA 3-1
 }
 
+#define mdebug(msg, ...) printf(msg, __VA_ARGS__)
+
 // read data from cache
 uint32_t cache_read(paddr_t paddr, size_t len)
 {
