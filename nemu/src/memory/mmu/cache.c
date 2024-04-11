990a2fba3746f35d14cd361e0339a4f59570c79f
tracer-pa-public <tracer@njuics.org>
2024-04-11 17:04:41 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 17:04:41 up 2 days,  7:36,  6 users,  load average: 0.05, 0.02, 0.00 > unitime > da904453aee355c219c7cf199659ba1130879486

--- nemu/src/memory/mmu/cache.c
+++ nemu/src/memory/mmu/cache.c
@@ -78,7 +78,7 @@ uint32_t cache_read(paddr_t paddr, size_t len)
 		}
 	}
 	// 缓存替换
-	uint32_t index = group * 8 + paddr & ((1<<3)-1);
+	uint32_t index = group * 8 + (paddr & ((1<<3)-1));
 	cache_line_info[index].valid = 1;
 	cache_line_info[index].tag = tag;
 	memcpy(cache_line_data[index].data, hw_mem + BASEADDR64(paddr), CACHE_LINE_SIZE);
