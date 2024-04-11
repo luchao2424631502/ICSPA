19156d0ceff16ff11e1c9343ea2cbb15e277484b
tracer-pa-public <tracer@njuics.org>
2024-04-11 22:16:55 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 22:16:55 up 2 days, 12:49,  8 users,  load average: 0.21, 0.16, 0.11 > unitime > f3e5a1a9317aa40bc9c07fc9691de9557a068214

--- nemu/src/memory/mmu/cache.c
+++ nemu/src/memory/mmu/cache.c
@@ -68,7 +68,8 @@ uint32_t cache_read(paddr_t paddr, size_t len)
 		// uint32_t ret1 = hw_mem_read(paddr, tmp);
 		uint32_t ret2 = cache_read(paddr + tmp, len - tmp);
 		// uint32_t ret2 = hw_mem_read(paddr + tmp, len - tmp);
-		printf("paddr=0x%x tmp=%x ret1=0x%x paddr2=0x%x tmp=%d ret2=0x%x\n", paddr, tmp, ret1, paddr+tmp, len-tmp, ret2);
+		printf("paddr=0x%x tmp=%x ret1=0x%x paddr2=0x%x tmp=%d ret2=0x%x ans=0x%x\n", 
+				paddr, tmp, ret1, paddr+tmp, len-tmp, ret2, ret1|(ret2 << (8 * tmp)));
 		printf("ans=0x%x\n", ret1 | (ret2 << (8 * tmp)));
 		return ret1 | (ret2 << (8 * tmp));
 	}
