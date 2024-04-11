087b45bd75c9c36cbfb78bfbb3875f902425ba73
tracer-pa-public <tracer@njuics.org>
2024-04-11 22:09:22 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 22:09:22 up 2 days, 12:41,  8 users,  load average: 0.21, 0.12, 0.12 > unitime > 27cdb06afdedc286db6243c64c0b74fa2b631ec1

--- nemu/src/memory/mmu/cache.c
+++ nemu/src/memory/mmu/cache.c
@@ -62,11 +62,12 @@ uint32_t cache_read(paddr_t paddr, size_t len)
 	// implement me in PA 3-1 
 	uint32_t tmp = is_span(paddr, len);
 	if (tmp) {
-		printf("[%s] span paddr=0x%x\n", __func__, paddr);
-		assert(0);
-		//uint32_t ret1 = cache_read(paddr, tmp);
-		//uint32_t ret2 = cache_read(paddr + tmp, len - tmp);
-		// return ret1 | (ret2 << (8 * tmp));
+		// printf("[%s] span paddr=0x%x\n", __func__, paddr);
+		// assert(0);
+		uint32_t ret1 = cache_read(paddr, tmp);
+		uint32_t ret2 = cache_read(paddr + tmp, len - tmp);
+		printf("ret1=0x%x ret2=0x%x\n", ret1, ret2);
+		return ret1 | (ret2 << (8 * tmp));
 	}
 	uint32_t tag = cache_get_tag(paddr);
 	uint32_t group = cache_get_group(paddr);
