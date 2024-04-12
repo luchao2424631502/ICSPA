3117405f2cfa787bdfb96de9a6deab7926dcf255
tracer-pa-public <tracer@njuics.org>
2024-04-12 10:32:28 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 10:32:28 up 3 days,  1:04,  8 users,  load average: 0.01, 0.05, 0.00 > unitime > 38ef6a556ad9c5ad7b5a33b5442d852931929dfe

--- nemu/src/memory/mmu/cache.c
+++ nemu/src/memory/mmu/cache.c
@@ -56,14 +56,15 @@ void cache_write(paddr_t paddr, size_t len, uint32_t data)
 	// implement me in PA 3-1
 	uint32_t tmp = is_span(paddr, len);
 	if (tmp) {
-		data = 0xe78af3d1;
+		// data = 0xe78af3d1;
 		uint32_t val1 = data & ((1<<(8 * tmp)) - 1);
 		uint32_t val2 = (data & ~((1<<(8 * tmp)) - 1)) >> (8 * tmp);
 		
 		printf("[%s] span paddr=0x%x tmp=%d data=0x%x val1=0x%x val2=0x%x\n", 
 				__func__, paddr, tmp, data, val1, val2);
-		assert(0);
-		// cache_write(paddr, tmp, data);
+		cache_write(paddr, tmp, val1);
+		cache_write(paddr + tmp, len - tmp, val2);
+		// assert(0);
 		//cache_write(paddr + tmp, len - tmp, data);
 		return ;
 	}
