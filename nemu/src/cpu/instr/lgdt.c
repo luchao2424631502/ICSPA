0b548b0c1d7d4d10b619f77fa23225044e205f33
tracer-pa-public <tracer@njuics.org>
2024-04-13 16:52:01 +0800

    > clean > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 16:52:01 up 4 days,  7:24,  6 users,  load average: 0.08, 0.10, 0.04 > unitime > d709faf95d5e0af327219f043d50be9520cff764

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -13,11 +13,11 @@ make_instr_func(lgdt) // 将地址指向的6字节载入GDTR寄存器中
 	uint16_t limit = vaddr_read(src.addr, SREG_CS, 2);
 	uint32_t base  = vaddr_read(src.addr + 2, SREG_CS, 4); 
 
-	{printf("[LGDT] limit=0x%x BASE=0x%x\n", limit, base);}
-
-	// 加载进GDTR寄存器中
+	// 将值加载进入GDTR中
 	cpu.gdtr.limit = limit;
 	cpu.gdtr.base = base;
+
+	{printf("[LGDT] limit=0x%x BASE=0x%x\n", limit, base);}
 
 	return len; 
 }
