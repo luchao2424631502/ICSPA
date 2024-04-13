f6dc0534f82bc2ff73d2e94951f264e2c0ba36c4
tracer-pa-public <tracer@njuics.org>
2024-04-13 16:47:22 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 16:47:22 up 4 days,  7:19,  6 users,  load average: 0.11, 0.08, 0.03 > unitime > 9e4b7d0fdad799af7ddc08fe6508e8e148827fd

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -2,3 +2,22 @@
 /*
 Put the implementations of `lgdt' instructions here.
 */
+make_instr_func(lgdt) // 将地址指向的6字节载入GDTR寄存器中
+{
+	int len = 1;
+	OPERAND src;
+	src.data_size = data_size;
+	
+	len += modrm_rm(eip + 1, &src);
+	// 源操作数值, 位于src.addr中
+	uint16_t limit = vaddr_read(src.addr, SREG_CS, 2);
+	uint32_t base  = vaddr_read(src.addr + 2, SREG_CS, 4); 
+
+	{printf("[LGDT] limit=0x%x BASE=0x%x\n", limit, base);}
+
+	// 加载进GDTR寄存器中
+	cpu.gdtr.limit = limit;
+	cpu.gdtr.base = base;
+
+	return len; 
+}
