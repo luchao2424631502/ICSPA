b1118b3c7c2772e25a2ba66f16aacf6cf9c88e2c
tracer-pa-public <tracer@njuics.org>
2024-04-17 20:31:26 +0800

    > clean > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 20:31:26 up 1 day,  8:21,  6 users,  load average: 0.00, 0.05, 0.07 > unitime > a2510a11844520fb37af6d1cace682176802e6aa

--- nemu/src/cpu/instr/lidt.c
+++ nemu/src/cpu/instr/lidt.c
@@ -2,3 +2,20 @@
 /*
 Put the implementations of `lidt' instructions here.
 */
+make_instr_func(lidt)
+{
+	int len = 1;
+	OPERAND src;
+	src.data_size = data_size;
+
+	len += modrm_rm(eip + 1, &src);
+	uint16_t limit = vaddr_read(src.addr, SREG_CS, 2);
+	uint32_t base  = vaddr_read(src.addr + 2, SREG_CS, 4);
+
+	cpu.idtr.limit = limit;
+	cpu.idtr.base = base;
+
+	{printf("[LIDT] limit=0x%x BASE=0x%x\n", limit, base);}
+
+	return len;
+}
