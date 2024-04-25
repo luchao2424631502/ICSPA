41e2399a1fe9c762bc29c017e321e666202759eb
tracer-pa-public <tracer@njuics.org>
2024-04-25 11:04:50 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 11:04:50 up 17:47,  5 users,  load average: 0.01, 0.04, 0.00 > unitime > 4e95b1d665660943f6f720d74e8e810385883416

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -31,7 +31,8 @@ void raise_intr(uint8_t intr_no)
 		cpu.eflags.IF = 0;
 
 	// 2.查询IDT, 获得中断处理程序的入口地址
-	uint32_t idt_base = cpu.idtr.base;
+	uint32_t nemu_idt_base = cpu.idtr.base - KOFFSET + hw_mem;
+	
 	pd("idt_base", idt_base);
 	
 	printf("Please implement raise_intr()");
