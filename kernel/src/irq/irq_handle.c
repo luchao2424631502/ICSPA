7aaf16d6d0271a6d89b4cda8f3084abdd5bf8974
tracer-pa-public <tracer@njuics.org>
2024-04-25 18:43:39 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 18:43:39 up 1 day,  1:26,  6 users,  load average: 0.05, 0.03, 0.00 > unitime > a16ea897e3f9f6ca1c2b96a94233185737631332

--- kernel/src/irq/irq_handle.c
+++ kernel/src/irq/irq_handle.c
@@ -44,7 +44,7 @@ void irq_handle(TrapFrame *tf)
 	}
 	else if (irq < 1000)
 	{
-		panic("Unexpected exception #%d at eip = %x\n", irq, tf->eip);
+		panic("Unexpected exception #%d at eip = %x tf=0x%x esp=0x%x\n", irq, tf->eip, (uint32_t)tf, cpu.esp);
 	}
 	else if (irq >= 1000)
 	{
