8f93fa2affa41c8501cf27d64ff2378e391f5547
tracer-pa-public <tracer@njuics.org>
2024-04-25 10:17:34 +0800

    > clean > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 10:17:34 up 17:00,  5 users,  load average: 0.00, 0.00, 0.00 > unitime > ce2eeea8cdef28e56ba5379b15188cc4ee278090

--- nemu/src/cpu/instr/int_.c
+++ nemu/src/cpu/instr/int_.c
@@ -6,3 +6,21 @@ Put the implementations of `int' instructions here.
 
 Special note for `int': please use the instruction name `int_' instead of `int'.
 */
+make_func_name(int_)
+{
+	int len = 1;
+	OPERAND src;
+	src.data_size = 8; // ib
+	src.type = OPR_IMM;
+	src.sreg = SREG_CS;
+	src.addr = eip + 1;
+	len += src.data_size / 8;
+
+	// 0. 读取源操作数的值
+	operand_read(&src);
+	{printf("[int 0x%x] src.val=0x%x", src.val, src.val);}
+
+	// 1. 什么都不做
+	assert(0);
+	return len;
+}
