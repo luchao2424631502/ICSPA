ef97f1e01837489049b57de7e056a85c61d721f0
tracer-pa-public <tracer@njuics.org>
2024-04-27 14:21:50 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 14:21:50 up  5:14,  8 users,  load average: 0.21, 0.14, 0.22 > unitime > d4f3a34bd3d7bffb6bb3bfda2301eaa43646563d

--- nemu/src/cpu/instr/out.c
+++ nemu/src/cpu/instr/out.c
@@ -10,7 +10,7 @@ make_instr_func(out_b)
 	// 将AL reg传送到DX reg表示的io端口
 	pio_write(cpu.gpr[0x2]._16, 1, cpu.gpr[0x0]._8[0]);
 
-	printf("[OUT_B] AL=0x%x DX=0x%x\n", cpu.gpr[0x0]._8[0], cpu.edx);
+	printf("[OUT_B] AL=0x%x DX=0x%x\n", cpu.gpr[0x0]._8[0], cpu.gpr[0x2].16);
 
 	return len;
 }
