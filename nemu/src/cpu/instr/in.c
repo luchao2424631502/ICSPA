18a77cb46b924ffdecdbd5ec417b0e52be87ba18
tracer-pa-public <tracer@njuics.org>
2024-04-27 15:40:19 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 15:40:19 up  6:32,  8 users,  load average: 0.02, 0.02, 0.02 > unitime > 9ccecd9df916017c33bca7ad7d098263818b78

--- nemu/src/cpu/instr/in.c
+++ nemu/src/cpu/instr/in.c
@@ -16,3 +16,17 @@ make_instr_func(in_b)
 
 	return len;
 }
+
+make_instr_func(in_v)
+{
+	int len = 1;
+
+	// Dx reg表示的端口中读取数据到EAX reg
+	cpu.gpr[0x0]._32 = pio_read(cpu.gpr[0x2]._16, 4);
+
+	{printf("[IN_V] eax=0x%x dx=0x%x,\n", cpu.gpr[0x0]._32, cpu.gpr[0x02]._16);}
+
+	assert(0);
+
+	return len;
+}
