fdac51407051ef1cc2dbd166395c4ec284844bb6
tracer-pa-public <tracer@njuics.org>
2024-04-27 14:14:06 +0800

    > clean > id icspa_mooc_student > user luchao > uname Linux i3862 4.19.0-26-686-pae #1 SMP Debian 4.19.304-1 (2024-01-09) i686 GNU/Linux > uptime 14:14:06 up  5:06,  8 users,  load average: 0.12, 0.08, 0.28 > unitime > b14e081f35a66695552afa5552be3696ec1a0d0f

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -19,37 +19,37 @@ void load_sreg(uint8_t sreg)
 	uint16_t index = cpu.segReg[sreg].val >> 3; // 得到GDT表索引
 	SegDesc *entry = hwa_to_va(cpu.gdtr.base);
 	
-	{printf("[%s] index=%d\n", __func__, index);};
+	// {printf("[%s] index=%d\n", __func__, index);};
 
 	// 从GDTR加载到seg reg cache中
 	// 1. base
 	uint32_t base = entry[index].base_15_0 | (entry[index].base_23_16 << 16) | 
 		(entry[index].base_31_24 << 24);
-	{printf("[%s] base=0x%X\n", __func__, base);}
+	// {printf("[%s] base=0x%X\n", __func__, base);}
 	assert(0 == base);
 	cpu.segReg[sreg].base = base;
 
 	// 2. limit
 	uint32_t limit = entry[index].limit_15_0 | (entry[index].limit_19_16 << 16);
 	uint8_t granularity = entry[index].granularity; 
-	{printf("[%s] limit=0x%X granularity=%d\n", __func__, limit, granularity);}
+	// {printf("[%s] limit=0x%X granularity=%d\n", __func__, limit, granularity);}
 	assert(0xFFFFF == limit);
 	assert(0x1 == granularity);
 	cpu.segReg[sreg].limit = limit;
 
 	// 3. type
 	uint8_t type = entry[index].type;
-	{printf("[%s] type=0x%X\n", __func__, type);}
+	// {printf("[%s] type=0x%X\n", __func__, type);}
 	cpu.segReg[sreg].type = type;
 	
 	// 4. privilege_level
 	uint8_t privilege_level = entry[index].privilege_level;
-	{printf("[%s] privilege_level=0x%X\n", __func__, privilege_level);}
+	// {printf("[%s] privilege_level=0x%X\n", __func__, privilege_level);}
 	cpu.segReg[sreg].privilege_level = privilege_level;
 
 	// 5. soft_use
 	uint8_t soft_use = entry[index].soft_use;
-	{printf("[%s] soft_use=0x%X\n", __func__, soft_use);}
+	// {printf("[%s] soft_use=0x%X\n", __func__, soft_use);}
 	cpu.segReg[sreg].soft_use = soft_use;
 
 	return ;
